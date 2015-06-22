1a3ef68cd02c5e25b63a7607423e676e21cc28e0
Chris Lee <c_leeonidas41@yahoo.com>
2015-06-21 20:18:10 -0400

    assignment-32-SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,27 @@ require 'faker'
      body: Faker::Lorem.paragraph
    )
  end
+
+ post = [
+  {title: "unique title check", body: "unique body check"}
+ ]
  
+ post.each do |post|
+    Post.where(post).first_or_create
+end
+
+comment = [
+  {post: "unique post", body: "unique body comment check"}
+]
+
+comment.each do |comment|
+  Comment.where(comment).first_or_create
+end
+
+
+
+
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
