8ef734f2c247f6019a76dfefdc9c33118b3fee8c
Chris Lee <c_leeonidas41@yahoo.com>
2015-06-25 11:57:06 -0400

    assignment-39-Authorization

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,16 @@
 class PostPolicy < ApplicationPolicy
 
+  class Scope < Scope
+    def resolve
+      if user.present? && (user.admin? || user.moderator?)
+        scope.all
+      else
+        scope.where(:user => user)
+      end
+    end
+  end
+
+
   def index?
     true
   end
