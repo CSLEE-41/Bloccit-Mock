401b6fa79324627a83371844bf4e007b8e51208a
Chris Lee <c_leeonidas41@yahoo.com>
2015-06-26 14:51:47 -0400

    checkpoint-40-Topic

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,15 @@ require 'faker'
  end
  users = User.all
 
+#Create Topics
+15.times do
+  Topic.create!(
+      name: Faker::Lorem.sentence,
+      description: Faker::Lorem.paragraph
+    )
+end
 
+topics = Topic.all
 
 
 
@@ -22,6 +30,7 @@ require 'faker'
  50.times do
    Post.create!(
      user:   users.sample,
+     topic: topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -39,12 +48,34 @@ require 'faker'
  end
  
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'c_leeonidas41@yahoo.com',
+  # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+ member.skip_confirmation!
+ member.save!
 
 
  puts "Seed finished"
