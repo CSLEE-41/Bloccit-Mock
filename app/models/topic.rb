03eab120c51180761adf6b543b2f880783f4c7e5
Chris Lee <c_leeonidas41@yahoo.com>
2015-07-01 09:56:23 -0400

    assignment-45-paginate

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts
+
+  self.per_page = 50
+
 end
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
  # Create Users
- 5.times do
+ 50.times do
    user = User.new(
      name:     Faker::Name.name,
      email:    Faker::Internet.email,
@@ -13,7 +13,7 @@ require 'faker'
  users = User.all
 
 #Create Topics
-15.times do
+100.times do
   Topic.create!(
       name: Faker::Lorem.sentence,
       description: Faker::Lorem.paragraph
@@ -27,7 +27,7 @@ topics = Topic.all
 
 
 # Create Posts
- 50.times do
+ 1000.times do
    Post.create!(
      user:   users.sample,
      topic: topics.sample,
@@ -39,7 +39,7 @@ topics = Topic.all
  posts = Post.all
  
  # Create Comments
- 100.times do
+ 500.times do
    Comment.create!(
       # user: users.sample,   # we have not yet associated Users with Comments
      post: posts.sample,
