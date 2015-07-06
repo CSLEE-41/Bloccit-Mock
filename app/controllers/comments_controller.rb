d9f12deb15adbd1ca50133ed7f581a237b60f816
Chris Lee <c_leeonidas41@yahoo.com>
2015-07-06 17:33:54 -0400

    assignment-46-comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
+    authorize @comment
       
 
 
