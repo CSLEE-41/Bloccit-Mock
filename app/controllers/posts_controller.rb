cbe8a6135b858c584d76a46355cb5fe5ef6b4a37
Chris Lee <c_leeonidas41@yahoo.com>
2015-07-09 18:35:15 -0400

    checkpoint-50-MockRspec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@
      authorize @post
 
      if @post.save
+       @post.create_vote
        flash[:notice] = "Post was saved."
        redirect_to [@topic, @post]
      else
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,10 +9,10 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
-  after_create :create_vote
+  
 
   def up_votes
     votes.where(value: 1).count
@@ -34,7 +34,7 @@ class Post < ActiveRecord::Base
   end
 
 
-  private
+
 
   def create_vote
     user.votes.create(value: 1, post: self)
