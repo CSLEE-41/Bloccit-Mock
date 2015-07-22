a93f363f77e8a8ae220a782a7092093512e42c15
Chris Lee <c_leeonidas41@yahoo.com>
2015-07-21 22:07:50 -0400

    assignment-56-Testing

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,4 +6,15 @@
      password_confirmation "helloworld"
      confirmed_at Time.now
    end
+
+
+   factory :user_with_post_and_comment, parent: :user do
+    transient do
+      comments_count 1    
+    end
+    after(:build) do |user, evaluator|
+      post = create(:post, user: user)
+      create_list(:comment, evaluator.comments_count, user: user, post: post)
+    end
+  end
  end
